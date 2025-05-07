fe65dcefe09e48f9dcb5a644fb3a9c8b33960d19
Talha Hameed Khan <talhahameedkhan@Talhas-MacBook-Air.local>
2025-05-08 02:21:00 +0800

    Cleaned code, removed duplicates, updated read me.md

--- MCPServer/README.md
+++ MCPServer/README.md
@@ -1,1 +1,8 @@
+Paste this to terminal
+open virtual env
+pip install -r requirements.txt
+uv run mcp dev MCPServer/functions.py
+
+
+
 
--- Main.py
+++
@@ -1,7 +0,0 @@
-import functions
-
-
-# Start Strategy
-functions.start_strategy(strategy=["RSI"], symbol="ETHUSDT", interval="1m", candletime="UTC+800")
-
-
